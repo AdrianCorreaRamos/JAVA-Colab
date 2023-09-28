c8a7dfda07822c15afb440daabf49db739fa08c0
Liz Correa <acorrear001@alumno.uaemex.mx>
2023-09-28 10:12:31 -0600

    Added:     + classes         Sum     + Methods         calculate     + Variables         num1         num2         result

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -5,5 +7,19 @@ public class Main {
          System.out.println("soy esme!");
         System.out.println("Y yo Adrian");
         System.out.println("Y yo Carmen");
+        double num1;
+        double num2;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingresa un numero");
+        num1 = in.nextDouble();
+        System.out.println("Ingresa un segundo numero");
+        num2 = in.nextDouble();
+
+        Sum sum = new Sum(num1, num2);
+        double result = sum.calculate();
+
+        System.out.println("La suma es: " + result);
+
     }
 }
